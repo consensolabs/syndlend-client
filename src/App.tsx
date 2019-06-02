2b53ca5710d449722e22c627a7322b4256e2cd16
JaswanthReddyV <vutukurijaswanth515@gmail.com>
2019-06-02 18:15:42 +0530

    Creates Initial files

---
+++ src/App.tsx
@@ -0,0 +1,177 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./index.css";
+import './App.css';
+
+import { Layout, Menu, Breadcrumb, Icon, Button } from 'antd';
+import { Table, Divider, Tag } from 'antd';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+class App extends React.Component {
+  state = {
+    collapsed: false
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+
+  render() {
+    return (
+      <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  subnav 1
+                </span>
+              }
+            >
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="laptop" />
+                  subnav 2
+                </span>
+              }
+            >
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3"
+              title={
+                <span>
+                  <Icon type="notification" />
+                  subnav 3
+                </span>
+              }
+            >
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              background: '#fff',
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            <h2> Dashboard </h2>
+
+            <div>
+              <div className="requests-bar">
+              <span style={{
+                margin: '1em 0em',
+                fontSize: '1.17em'
+              }}> Loan Requests </span>
+              
+              <Button type="dashed"> + ADD REQUEST </Button>
+              </div>
+              <Table dataSource={dataSource} columns={columns} />
+            </div>
+
+            <div>
+              <span style={{
+                margin: '1em 0em',
+                fontSize: '1.17em'
+              }}> Loan Requests </span>
+
+              <Table dataSource={dataSource} columns={columns} />
+            </div>
+
+            {/* <div>
+              <span style={{
+                margin: '1em 0em',
+                fontSize: '1.17em'
+              }}> Loan Requests </span>
+
+              <Table dataSource={dataSource} columns={columns} />
+            </div> */}
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+    );
+  }
+}
+
+export default App;
