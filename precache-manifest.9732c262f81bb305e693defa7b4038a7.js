7a0fa11c29c7697b9b0cc3bbc2ee3ab57ecf2dfe
koshikraj <koshik.raj@gmail.com>
2019-06-12 13:56:02 +0530

    adds first version of the build for demo

---
+++ precache-manifest.9732c262f81bb305e693defa7b4038a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ca63a769a90b56d2f1c808bc6737c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "295dc2f4ca02a1c624ba",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "e4b942fc9b461ae3a3cb",
+    "url": "/static/css/main.7b6f68e8.chunk.css"
+  },
+  {
+    "revision": "295dc2f4ca02a1c624ba",
+    "url": "/static/js/2.04b31817.chunk.js"
+  },
+  {
+    "revision": "e4b942fc9b461ae3a3cb",
+    "url": "/static/js/main.d26f7057.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
